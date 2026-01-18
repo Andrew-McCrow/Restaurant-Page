2dde45cb6edb4ac92813cefa29a723b37a74f7f4
Andrew-McCrow <andrewmccrow@gmail.com>
2026-01-18 19:22:31 +0000

    updated page content and UI

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,14 @@
+import restaurantImage from '../images/restaurant.jpg';
+
+function homePageContent() {
+
 // Select the div#content element
 const homePageContent = document.getElementById('content');
 
 // Add heading to the content div
-const homeContent = document.createElement('h1');
-homeContent.textContent = "Welcome to our Restaurant Page!";
-homePageContent.appendChild(homeContent);
+const heading = document.createElement('h1');
+heading.textContent = "Welcome to our Restaurant!";
+homePageContent.appendChild(heading);
 
 // Add a paragraph to the content div
 const description = document.createElement('p');
@@ -12,11 +16,10 @@ description.textContent = "Experience the finest dining with us.";
 homePageContent.appendChild(description);
 
 // Add an image to the content div
-import restaurantImage from '/src/images/restaurant.jpg';
 const imageElement = document.createElement('img');
 imageElement.src = restaurantImage;
 imageElement.alt = "Delicious food at our restaurant";
 homePageContent.appendChild(imageElement);
-
+}
 // export the contentDiv for use in other modules
 export default homePageContent;
